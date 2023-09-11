1f5ce358956ed3438d01eca7b288ae2858c74898
Noriuki <lanezupiroli@gmail.com>
2023-09-11 16:34:56 -0300

    fix: :bug: fix deploy

--- src/seeds/seeder.module.ts
+++ src/seeds/seeder.module.ts
@@ -4,11 +4,9 @@ import { CategoriesModule } from 'src/modules/categories/categories.module';
 import { OrdersModule } from 'src/modules/orders/orders.module';
 import { ProductsModule } from 'src/modules/products/products.module';
 import { SeedService } from './seeder';
-import { SeederController } from './seeder.controller';
 
 @Module({
   imports: [AdditionalsModule, CategoriesModule, ProductsModule, OrdersModule],
-  controllers: [SeederController],
   providers: [SeedService],
 })
 export class SeederModule {}
--- src/seeds/seeder.ts
+++ src/seeds/seeder.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import { AdditionalsService } from 'src/modules/additionals/additionals.service';
 import { CreateAdditionalDto } from 'src/modules/additionals/dto/create-additional.dto';
 import { CategoriesService } from 'src/modules/categories/categories.service';
@@ -6,14 +6,14 @@ import { CreateProductDto } from 'src/modules/products/dto/create-product.dto';
 import { ProductsService } from 'src/modules/products/products.service';
 
 @Injectable()
-export class SeedService {
+export class SeedService implements OnApplicationBootstrap {
   constructor(
     private readonly categoriesService: CategoriesService,
     private readonly additionalsService: AdditionalsService,
     private readonly productsService: ProductsService,
   ) {}
 
-  async seed() {
+  async   onApplicationBootstrap(): Promise<void> {
     const logger = new Logger('Seeding');
     logger.log('--- Seed Service: Check ---');
     await this.categoriesSeeder();
